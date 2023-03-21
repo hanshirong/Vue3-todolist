d04f93b4906b76cf7e6c511779124cced5bfc5cc
hanshirong <1418264042@qq.com>
2023-03-21 12:38:43 +0800

    基础的动态删除功能

---
+++ todolist-app/src/components/ToDoList.vue
@@ -0,0 +1,72 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <ul class="ToDoList">
+    <li v-for="(item,index) in items" :key="item.id">
+        <span v-text="item.title"></span>
+        <button v-on:click="deleteItem(item.id)">完成</button>
+    </li>
+  </ul>
+
+</template>
+
+<script lang="ts">
+import { defineComponent,ref,reactive } from 'vue';
+
+export default defineComponent({
+  name: 'ToDoList',
+  props: {
+    msg: String,
+  },
+  setup(){
+    const items = reactive([
+      {
+        id:1,
+        title:"吃"
+      },
+      {
+        id:2,
+        title:"睡"
+      },
+      {
+        id:3,
+        title:"学习"
+      },
+    ])
+
+    let deleteItem = (id:number) => {
+      // 根据id删除item
+      items.forEach((item,index) =>{
+        if(item.id == id){
+          items.splice(index,1)
+        }
+      })
+      console.log(items)
+    }
+    
+    return{
+      items,
+      deleteItem
+    }
+
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  /* display: inline-block; */
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ todolist-app/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+    <ToDoList msg="todoList"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ToDoList from '@/components/ToDoList.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    ToDoList,
+  },
+});
+</script>
